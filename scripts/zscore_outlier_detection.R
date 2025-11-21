b4a473d655dfb5b0b36802a2c24e6845340b2735
mfras <mfrasunk@hotmail.com>
2025-11-21 18:09:33 +0100

    comparing predicted outliers using z-score and threshold to true outliers and eval with confusion matrix

---
+++ scripts/zscore_outlier_detection.R
@@ -0,0 +1,70 @@
+library(here)
+library(readr)
+
+source(here("scripts", "ols.R"))
+
+# Define file paths
+data_path <- here("data", "df_sim_4.txt")
+save_path <- here("outputs", "signal_vs_concentration.png")
+
+# Load data
+df <- read_table(data_path)
+
+# make containers
+conc <- unique(df$conc)
+median_all <- numeric(length(conc))
+mad_all <- numeric(length(conc))
+mean_all <- numeric(length(conc))
+sd_all <- numeric(length(conc))
+
+# fill containers
+for(i in seq_along(conc)) {
+  idx <- df$conc == conc[i]
+  median_all[i] <- median(df$signal_out[idx])
+  mad_all[i] <- mad(df$signal_out[idx])
+  mean_all[i] <- mean(df$signal_out[idx])
+  sd_all[i] <- sd(df$signal_out[idx])
+}
+
+# z-scores
+z_score_classic <- numeric(nrow(df))
+z_score_robust <- numeric(nrow(df))
+
+for(i in seq_along(conc)) {
+  idx <- df$conc == conc[i]
+  
+  z_score_classic[idx] <- (df$signal_out[idx] - mean_all[i]) / sd_all[i]
+  
+  z_score_robust[idx] <- (df$signal_out[idx] - median_all[i]) / mad_all[i]
+}
+
+idx <- df$conc == conc[1]
+plot(z_score_robust[idx])
+abline(h=3)
+
+# determine outliers
+outlier_classic <- z_score_classic >= 3
+outlier_robust <- z_score_robust >= 3
+
+# get prediction stats
+TP_classic <- sum(outlier_classic & df$is_outlier)
+FP_classic <- sum(outlier_classic & !df$is_outlier)
+FN_classic <- sum(!outlier_classic & df$is_outlier)
+TN_classic <- sum(!outlier_classic & !df$is_outlier)
+
+TP_robust <- sum(outlier_robust & df$is_outlier)
+FP_robust <- sum(outlier_robust & !df$is_outlier)
+FN_robust <- sum(!outlier_robust & df$is_outlier)
+TN_robust <- sum(!outlier_robust & !df$is_outlier)
+
+# alternatively use package
+library(caret)
+
+predicted <- factor(c(outlier_robust))
+actual <- factor(c(df$is_outlier))
+
+conf_matrix <- confusionMatrix(predicted, actual)
+
+print(conf_matrix)
+
+
